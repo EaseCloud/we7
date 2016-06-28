c0325f37b15ae16683dba83cfdd172f938227b66
Alfred Huang <57082212@qq.com>
2016-06-28 23:24:57 +0800

    将二开目录从芸众插件中移出，以便日后将芸众模块子模块化

--- addons/sz_yi/sdk/index.php
+++ addons/sz_yi/sdk/index.php
@@ -1,11 +1,11 @@
 <?php
 
 define('SDK_ROOT', SZ_YI_PATH . 'sdk/');
-define('DEVELOP_ROOT', SZ_YI_PATH . 'develop/');
+define('DEVELOP_ROOT', IA_ROOT . '/develop/');
 
 require_once 'debugger.php';
 require_once 'hook.php';
 
-if (is_file(__DIR__ . '/../develop/index.php')) {
-    require_once __DIR__ . '/../develop/index.php';
+if (is_file(DEVELOP_ROOT . 'index.php')) {
+    require_once DEVELOP_ROOT . 'index.php';
 } 
